ee0b8118c8c9e42cbafe2916b7378142c520df9e
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-11 10:46:52 -0300

    Added imput search and trailer api

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const KEY = 'k_mye57d2o';
+const KEY1 = 'k_mye57d2o';
+// Fazer função para alternar entre as chaves ou trocar manualmente?
+const KEY2 = 'k_24fzarfk';
 
 const burgerBtn = document.querySelector('.burger-icon');
 
@@ -9,8 +11,21 @@ const requestOptions = {
 
 const fetchAPI = async (genres, count) => {
   try {
-    const URL = `https://imdb-api.com/API/AdvancedSearch/${KEY}/?genres=${genres}&count=${count}`;
+    const URL = `https://imdb-api.com/API/AdvancedSearch/${KEY1}/?genres=${genres}&count=${count}`;
     const response = await fetch(URL, requestOptions);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// imDbId: "tt1375666" - Exemplo ID do filme;
+// Acessar "link": "https://www.imdb.com/video/vi2959588889" no mesmo objeto;
+const fetchAPITrailer = async (filme) => {
+  try {
+    const URLTRAILER = `https://imdb-api.com/en/API/Trailer/k_24fzarfk/${filme}`;
+    const response = await fetch(URLTRAILER, requestOptions);
     const data = await response.json();
     console.log(data);
   } catch (error) {
