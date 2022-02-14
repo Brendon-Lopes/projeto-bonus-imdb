9221a2a416a7298a2480c30ec565dc06925acdde
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-14 18:16:29 -0300

    :bug:

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// const KEY1 = 'k_mye57d2o';
-// Fazer função para alternar entre as chaves ou trocar manualmente?
-const KEY1 = 'k_24fzarfk';
+const KEY1 = 'k_mye57d2o';
+// const KEY1 = 'k_24fzarfk';
 // const KEY1 = 'k_xj52n88c';
 const burgerEvent = document.querySelector('.burger-icon');
 const board = document.querySelector('.board');
@@ -12,6 +11,32 @@ const requestOptions = {
   method: 'GET',
   redirect: 'follow'
 };
+
+const fetchAPI = async (groups, genres) => {
+  try {
+    const URL = `https://imdb-api.com/API/AdvancedSearch/${KEY1}/?genres=${genres}&groups=${groups}`;
+    const response = await fetch(URL, requestOptions);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// imDbId: "tt1375666" - Exemplo ID do filme;
+// Acessar "link": "https://www.imdb.com/video/vi2959588889" no mesmo objeto;
+const fetchAPITrailer = async (filme) => {
+  try {
+    const URLTRAILER = `https://imdb-api.com/en/API/Trailer/${KEY1}/${filme}`;
+    const response = await fetch(URLTRAILER, requestOptions);
+    const data = await response.json();
+    const link = data.link;
+    return link;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 const createTrailer = async (section) => {
   const idMovie = section.id;
